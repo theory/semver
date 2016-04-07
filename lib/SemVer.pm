78b22dfdf16fd132afd1b76e41534fed9f47b298
hoppfrosch <hoppfrosch@gmx.de>
2016-04-07 15:24:44 +0200

    theory/semver/#2 - Updated POD for SemVer 2.0.0 Specification
    - Made all unittests work again

--- lib/SemVer.pm
+++ lib/SemVer.pm
@@ -12,7 +12,7 @@ use overload (
 );
 
 our @ISA = qw(version);
-our $VERSION = '0.8.0-alpha.3'; # For Module::Build
+our $VERSION = '0.8.0'; # For Module::Build
 
 sub _die { require Carp; Carp::croak(@_) }
 
@@ -220,12 +220,12 @@ SemVer - Use semantic version numbers
 
 =head1 Synopsis
 
-  use SemVer; our $VERSION = SemVer->new('1.2.0b1');
+  use SemVer; our $VERSION = SemVer->new('1.2.0-b1');
 
 =head1 Description
 
 This module subclasses L<version> to create semantic versions, as defined by
-the L<Semantic Versioning 1.0.0 Specification|http://semver.org/spec/v1.0.0.html>.
+the L<Semantic Versioning 2.0.0 Specification|http://semver.org/spec/v2.0.0.html>.
 The two salient points of the specification, for the purposes of version
 formatting, are:
 
@@ -233,19 +233,30 @@ formatting, are:
 
 =item 1.
 
-A normal version number MUST take the form X.Y.Z where X, Y, and Z are
-integers. X is the major version, Y is the minor version, and Z is the patch
-version. Each element MUST increase numerically by increments of one. For
-instance: C<< 1.9.0 < 1.10.0 < 1.11.0 >>.
+A normal version number MUST take the form X.Y.Z where X, Y, and Z are non-negative 
+integers, and MUST NOT contain leading zeroes. X is the major version, Y is the 
+minor version, and Z is the patch version. Each element MUST increase numerically. 
+For instance: C<< 1.9.0 -> 1.10.0 -> 1.11.0 >>.
 
 =item 2.
 
-A pre-release version number MAY be denoted by appending an arbitrary string
-immediately following the patch version and a dash. The string MUST be
-comprised of only alphanumerics plus dash C<[0-9A-Za-z-]>. Pre-release
-versions satisfy but have a lower precedence than the associated normal
-version. Precedence SHOULD be determined by lexicographic ASCII sort order. For
-instance: C<< 1.0.0-alpha1 < 1.0.0-beta1 < 1.0.0-beta2 < 1.0.0-rc1 < 1.0.0 >>.
+A pre-release version MAY be denoted by appending a hyphen and a series of dot 
+separated identifiers immediately following the patch version. Identifiers MUST 
+comprise only ASCII alphanumerics and hyphen C<[0-9A-Za-z-]>. Identifiers MUST NOT 
+be empty. Numeric identifiers MUST NOT include leading zeroes. Pre-release versions 
+have a lower precedence than the associated normal version. A pre-release version 
+indicates that the version is unstable and might not satisfy the intended 
+compatibility requirements as denoted by its associated normal version: 
+C<< 1.0.0-alpha, 1.0.0-alpha.1, 1.0.0-0.3.7, 1.0.0-x.7.z.92 >>
+
+=item 3.
+
+Build metadata MAY be denoted by appending a plus sign and a series of dot separated 
+identifiers immediately following the patch or pre-release version. Identifiers MUST 
+comprise only ASCII alphanumerics and hyphen C<[0-9A-Za-z-]>. Identifiers MUST NOT 
+be empty. Build metadata SHOULD be ignored when determining version precedence. Thus 
+two versions that differ only in the build metadata, have the same precedence. 
+Examples: C<< 1.0.0-alpha+001, 1.0.0+20130313144700, 1.0.0-beta+exp.sha.5114f85 >>.
 
 =back
 
