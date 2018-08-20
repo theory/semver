theory/semver

commit 8781c0d987f2abde08293a7ea127b68167bc9cb6

theory/semver/#2 - Enable strict SemVer in sub declare() and sub parse()
- Made all unittests work again

lib/SemVer.pm
@@ -12,7 +12,7 @@ use overload (
 );
 
 our @ISA = qw(version);
-our $VERSION = '0.8.0-alpha.2'; # For Module::Build
+our $VERSION = '0.8.0-alpha.3'; # For Module::Build
 
 sub _die { require Carp; Carp::croak(@_) }
 
@@ -83,14 +83,14 @@ sub declare {
     return $class->new($ival) if Scalar::Util::isvstring($ival)
         or eval { $ival->isa('version') };
 
-    (my $v = $ival) =~ s/(?:($DASH_SEPARATOR?)($OPTIONAL_EXTRA_PART))[[:space:]]*$//;
+    (my $v = $ival) =~ s/^v?$STRICT_DOTTED_INTEGER_VERSION(?:($DASH_SEPARATOR)($OPTIONAL_EXTRA_PART))[[:space:]]*$//;
     my $dash  = $1;
     my $extra = $2;
     $v += 0 if $v =~ s/_//g; # ignore underscores.
     my $self = $class->SUPER::declare($v);
     $self->{dash}  = $dash;
     $self->{extra} = $extra;
-    $self->_evalPreRelease($self->extra);
+    $self->_evalPreRelease($self->{extra});
     return $self;
 }
 
@@ -99,14 +99,14 @@ sub parse {
     return $class->new($ival) if Scalar::Util::isvstring($ival)
         or eval { $ival->isa('version') };
 
-    (my $v = $ival) =~ s/(?:($DASH_SEPARATOR?)($OPTIONAL_EXTRA_PART))[[:space:]]*$//;
+    (my $v = $ival) =~ s/^v?$STRICT_DOTTED_INTEGER_VERSION(?:($DASH_SEPARATOR)($OPTIONAL_EXTRA_PART))[[:space:]]*$//;
     my $dash  = $1;
     my $extra = $2;
     $v += 0 if $v =~ s/_//g; # ignore underscores.
     my $self = $class->SUPER::parse($v);
     $self->{dash}  = $dash;
     $self->{extra} = $extra;
-    $self->_evalPreRelease($self->extra);
+    $self->_evalPreRelease($self->{extra});
     return $self;
 }
 

t/base.t
@@ -2,7 +2,7 @@
 
 use strict;
 use warnings;
-use Test::More tests => 568;
+use Test::More tests => 610;
 #use Test::More 'no_plan';
 
 use FindBin qw($Bin);
@@ -120,18 +120,6 @@ eval { $version->numify };
 like $@, qr{Semantic versions cannot be numified},
     'Should get error from numify()';
 
-
-# Compare Versions from SemVer 2.0
-my @ver = ("0.9.0","1.0.0-alpha","1.0.0-alpha.1","1.0.0-alpha.beta","1.0.0-beta","1.0.0-beta.2","1.0.0-beta.11","1.0.0-beta.31","1.0.0-beta.200","1.0.0-rc.1","1.0.0","2.0.0","2.1.0","2.1.1");
-
-for (my $i = 0; $i < (scalar(@ver)-1); $i++) {
-  for (my $j = $i+1; $j < scalar(@ver); $j++) {
-    my $l = SemVer->new($ver[$i]);
-    my $r = SemVer->new($ver[$j]);
-    cmp_ok $l, '<', $r, "$l < $r";
-  }
-}
-
 # Now do some comparisons. Start with equivalents.
 for my $spec (
     [ '1.2.2',        '1.2.2' ],
@@ -201,6 +189,17 @@ for my $spec (
     cmp_ok $r, 'le', $l, "$r le $l";
 }
 
+# Compare Versions from SemVer 2.0
+my @ver = ("0.9.0","1.0.0-alpha","1.0.0-alpha.1","1.0.0-alpha.beta","1.0.0-beta","1.0.0-beta.2","1.0.0-beta.11","1.0.0-beta.31","1.0.0-beta.200","1.0.0-rc.1","1.0.0","2.0.0","2.1.0","2.1.1");
+
+for (my $i = 0; $i < (scalar(@ver)-1); $i++) {
+  for (my $j = $i+1; $j < scalar(@ver); $j++) {
+    my $l = SemVer->new($ver[$i]);
+    my $r = SemVer->new($ver[$j]);
+    cmp_ok $l, '<', $r, "$l < $r";
+  }
+}
+
 # Compare to version objects.
 my $semver = $CLASS->new('1.2.0');
 for my $v (qw(
@@ -231,15 +230,15 @@ for my $spec (
     ['01.2.2',         '1.2.2'],
     ['1.02.2',         '1.2.2'],
     ['1.2.02',         '1.2.2'],
-    ['1.2.02b',        '1.2.2-b'],
-    ['1.2.02beta-3  ', '1.2.2-beta-3'],
-    ['1.02.02rc1',     '1.2.2-rc1'],
+#    ['1.2.02b',        '1.2.2-b'],
+#    ['1.2.02beta-3  ', '1.2.2-beta-3'],
+#    ['1.02.02rc1',     '1.2.2-rc1'],
     ['1.0',            '1.0.0'],
     ['1.1',            '1.1.0',   '1.100.0'],
     [ 1.1,             '1.1.0',   '1.100.0'],
-    ['1.1b1',          '1.1.0-b1', '1.100.0-b1'],
-    ['1b',             '1.0.0-b'],
-    ['9.0beta4',       '9.0.0-beta4'],
+#    ['1.1b1',          '1.1.0-b1', '1.100.0-b1'],
+#    ['1b',             '1.0.0-b'],
+#    ['9.0beta4',       '9.0.0-beta4'],
     ['  012.2.2',      '12.2.2'],
     ['99999998',       '99999998.0.0'],
     ['1.02_30',        '1.23.0'],
@@ -252,7 +251,7 @@ for my $spec (
     ['9',              '9.0.0' ],
     ['0',              '0.0.0' ],
     [0,                '0.0.0' ],
-    ['0rc1',           '0.0.0-rc1' ],
+#    ['0rc1',           '0.0.0-rc1' ],
 ) { SKIP: {
         skip 'Two-integer vstrings weak on Perl 5.8', 12
             if $no_2digitvst && Scalar::Util::isvstring($spec->[0]);
@@ -300,3 +299,22 @@ for my $spec (
     }
 }}
 
+for my $v (qw(
+  1.2.02b
+  1.2.02beta-3
+  1.02.02rc1
+  1.1b1
+  1b
+  9.0beta4
+  0rc1
+)) {
+  eval {
+    my $l = SemVer->declare($v);
+  };
+  if ($@) {
+    is 1, 1,  $v.' is not a SemVer';
+  }
+  else {
+    is 1, 0,  $v.' should not be not a SemVer';
+  }
+}

commit 09fa772d6454e8230b5f6ea2752b61003c746fdf

Fix declare() with dash and extra
SemVer has a bug introduced in 8781c0d987f2abde08293a7ea127b68167bc9cb6:

  $ perl -MSemVer -le'print SemVer->declare("3.1.0")->normal'
  3.1.0

  $ perl -MSemVer -le'print SemVer->declare("3.1.0-something")->normal'
  Invalid version format (version required) at .../SemVer.pm line 90.

What happens in the second case ("3.1.0-something") is that this regular
expression matches:

  (my $v = $ival) =~ s/^v?$STRICT_DOTTED_INTEGER_VERSION(?:($DASH_SEPARATOR)($OPTIONAL_EXTRA_PART))[[:space:]]*$//;

So the entire version string is replaced by nothing, and that is the
value that $v gets. All other cases (such as "3.1", or "3.1.0") don't
match, and so the replacement never happens.

This empty $v is then passed on to version->declare, which complains
that the version is required.

It seems the full regexp was added hoping to be a validation step for
strict semantic versions in declare() and parse(). Unfortunately, this
wasn't working (as explained above), and in fact, that is not the
desired behaviour for these functions. From the docs for declare():
"This is the most flexible way to declare versions. Consider using it to
normalize version strings." With that in mind, the input has to allow
"not normal" version strings.

This patch restores the old behaviour, fixing usage of declare() and
parse(). I'm not super comfortable with how parse() is supposed to work,
so I didn't add any tests for it. But I did add tests for declare().

lib/SemVer.pm
@@ -83,7 +83,7 @@ sub declare {
     return $class->new($ival) if Scalar::Util::isvstring($ival)
         or eval { $ival->isa('version') };
 
-    (my $v = $ival) =~ s/^v?$STRICT_DOTTED_INTEGER_VERSION(?:($DASH_SEPARATOR)($OPTIONAL_EXTRA_PART))[[:space:]]*$//;
+    (my $v = $ival) =~ s/($DASH_SEPARATOR)($OPTIONAL_EXTRA_PART)$//;
     my $dash  = $1;
     my $extra = $2;
     $v += 0 if $v =~ s/_//g; # ignore underscores.
@@ -99,7 +99,7 @@ sub parse {
     return $class->new($ival) if Scalar::Util::isvstring($ival)
         or eval { $ival->isa('version') };
 
-    (my $v = $ival) =~ s/^v?$STRICT_DOTTED_INTEGER_VERSION(?:($DASH_SEPARATOR)($OPTIONAL_EXTRA_PART))[[:space:]]*$//;
+    (my $v = $ival) =~ s/($DASH_SEPARATOR)($OPTIONAL_EXTRA_PART)$//;
     my $dash  = $1;
     my $extra = $2;
     $v += 0 if $v =~ s/_//g; # ignore underscores.

t/base.t
@@ -2,7 +2,7 @@
 
 use strict;
 use warnings;
-use Test::More tests => 610;
+use Test::More tests => 699;
 #use Test::More 'no_plan';
 
 use FindBin qw($Bin);
@@ -146,6 +146,32 @@ for my $spec (
     cmp_ok $l, 'ge', $r, "$l ge $r";
 }
 
+# Test equivalents with declare
+for my $spec (
+    [ '1.2.0',                '1.2' ],
+    [ '0.0.0',                '0' ],
+    [ '999.888.7777-alpha.3', '999.888.7777-alpha.3' ],
+    [ '0.1.2-beta3',          '0.1.2-beta3' ],
+    [ '1.0.0-rc-1',           '1.0.0-RC-1' ],
+) {
+    my $l = $CLASS->declare($spec->[0]);
+    my $r = $CLASS->declare($spec->[1]);
+    is $l->vcmp($r), 0, "$l->vcmp($r) == 0";
+    is $l <=> $r, 0, "$l <=> $r == 0";
+    is $r <=> $l, 0, "$r <=> $l == 0";
+    cmp_ok $l, '==', $r, "$l == $r";
+    cmp_ok $l, '==', $r, "$l == $r";
+    cmp_ok $l, '<=', $r, "$l <= $r";
+    cmp_ok $l, '>=', $r, "$l >= $r";
+    is $l cmp $r, 0, "$l cmp $r == 0";
+    is $r cmp $l, 0, "$r cmp $l == 0";
+    cmp_ok $l, 'eq', $r, "$l eq $r";
+    cmp_ok $l, 'eq', $r, "$l eq $r";
+    cmp_ok $l, 'le', $r, "$l le $r";
+    cmp_ok $l, 'ge', $r, "$l ge $r";
+}
+
+
 # Test not equal.
 for my $spec (
     ['1.2.2',   '1.2.3'],
@@ -159,6 +185,24 @@ for my $spec (
 ) {
     my $l = $CLASS->new($spec->[0]);
     my $r = $CLASS->new($spec->[1]);
+    cmp_ok $l->vcmp($r), '!=', 0, "$l->vcmp($r) != 0";
+    cmp_ok $l, '!=', $r, "$l != $r";
+    cmp_ok $l, 'ne', $r, "$l ne $r";
+}
+
+# Test not equal with declare.
+for my $spec (
+    ['1.2.2',   '1.2.3'],
+    ['0.0.1',   '1.0.0'],
+    ['1.0.1',   '1.1.0'],
+    ['1.1.1',   '1.1.0'],
+    ['1.2.3-b', '1.2.3'],
+    ['1.2.3',   '1.2.3-b'],
+    ['1.2.3-a', '1.2.3-b'],
+    ['1.2.3-aaaaaaa1', '1.2.3-aaaaaaa2'],
+) {
+    my $l = $CLASS->declare($spec->[0]);
+    my $r = $CLASS->declare($spec->[1]);
     cmp_ok $l->vcmp($r), '!=', 0, "$l->vcmp($r) != 0";
     cmp_ok $l, '!=', $r, "$l != $r";
     cmp_ok $l, 'ne', $r, "$l ne $r";