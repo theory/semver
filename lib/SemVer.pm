8ac853be00e3aff88d6b1a7a0d64022210058697
David E. Wheeler <david@justatheory.com>
2020-05-08 14:21:38 -0400

    Increment to v0.10.0

--- lib/SemVer.pm
+++ lib/SemVer.pm
@@ -12,7 +12,7 @@ use overload (
 );
 
 our @ISA = qw(version);
-our $VERSION = '0.7.0'; # For Module::Build
+our $VERSION = '0.10.0'; # For Module::Build
 
 sub _die { require Carp; Carp::croak(@_) }
 
